8c0cd0c4a642016059533eb6e5355f0ad89e81fc
Gabriel Perez <gperez@arkusnexus.com>
2025-03-24 10:48:24 -0600

    Automatizar con Jenkins

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    video: true,
     //defaultCommandTimeout: 10000,
     baseUrl: "http://localhost:4200",
     setupNodeEvents(on, config) {
